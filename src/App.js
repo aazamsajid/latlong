9caf4c3998cbae75f8b5ee516c285a13217300fa
Azam Sajid <azamsajid@gmail.com>
2022-09-08 00:38:15 +0500

    fn

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import './App.css';
-import { useState } from 'react';
+import {  useState } from 'react';
 function App() 
 {
  const [lat, setLat] = useState(null);
  const [long, setLong] = useState(null);
  const geolocationAPI = navigator.geolocation;
  const url="https://google.com";
- const cnt =1;
- if (cnt > 0) {
  geolocationAPI.getCurrentPosition((position) => {
+
   const { coords } = position;
+
   setLat(coords.latitude);
   setLong(coords.longitude);
-  })
-  cnt = 0;
-}
+  
+});
+
+
 
 
 
@@ -24,7 +25,7 @@ return (
         <p>Your lat  is: {[lat]}</p>
         <p>Your long is w: {[long]}</p>
         <p>{lat},{long}</p>  
-
+        
       </div>
       );
 };
